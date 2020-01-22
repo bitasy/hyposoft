0752f1f4e979a5288533dc76d7f908f90739e039
Carter Gay <cag65@duke.edu>
2020-01-22 21:09:28 +0000

    create views

--- app/hyposoft/equipment/urls.py
+++ app/hyposoft/equipment/urls.py
@@ -1,4 +1,15 @@
 from django.urls import path
-from .views import *
+from rest_framework import routers
 
-urlpatterns = []
+#from .views import
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('equipment/eventCreate', EventListCreateView.as_view()),
+    path('reminders/eventDetail/<int:pk>', EventDetailView.as_view()),
+    path('reminders/reminderCreate', ReminderListCreateView.as_view()),
+    path('reminders/reminderDetail/<int:pk>', ReminderDetailView.as_view()),
+    path('reminders/allReminderList', AllReminderListView.as_view()),
+    path('reminders/allEventList', AllEventListView.as_view()),
+]
--- app/hyposoft/equipment/views.py
+++ app/hyposoft/equipment/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import ITModel, Instance, Rack
+from .serializers import ITModelSerializer, InstanceSerializer, RackSerializer
 
-# Create your views here.
+# CRUD, list views
+# ITModel
+class ITModelCreateView(generics.CreateAPIView):
+    queryset = ITModel.objects.all()
+    serializer_class = ITModelSerializer
+
+class ITModelRetrieveView(generics.RetrieveAPIView):
+    queryset = ITModel.objects.all()
+    serializer_class = ITModelSerializer
+
+class ITModelUpdateView(generics.UpdateAPIView):
+    queryset = ITModel.objects.all()
+    serializer_class = ITModelSerializer
+
+class ITModelDestroyView(generics.DestroyAPIView):
+    queryset = ITModel.objects.all()
+    serializer_class = ITModelSerializer
+
+class ITModelListView(generics.ListAPIView):
+    queryset = ITModel.objects.all()
+    serializer_class = ITModelSerializer
+
+# Instance
+class InstanceCreateView(generics.CreateAPIView):
+    queryset = Instance.objects.all()
+    serializer_class = InstanceSerializer
+
+class InstanceRetrieveView(generics.RetrieveAPIView):
+    queryset = Instance.objects.all()
+    serializer_class = InstanceSerializer
+
+class InstanceUpdateView(generics.UpdateAPIView):
+    queryset = Instance.objects.all()
+    serializer_class = InstanceSerializer
+
+class InstanceDestroyView(generics.DestroyAPIView):
+    queryset = Instance.objects.all()
+    serializer_class = InstanceSerializer
+
+class InstanceListView(generics.ListAPIView):
+    queryset = Instance.objects.all()
+    serializer_class = InstanceSerializer
+
+# Rack
+class RackCreateView(generics.CreateAPIView):
+    queryset = Rack.objects.all()
+    serializer_class = RackSerializer
+
+class RackRetrieveView(generics.RetrieveAPIView):
+    queryset = Rack.objects.all()
+    serializer_class = RackSerializer
+
+class RackUpdateView(generics.UpdateAPIView):
+    queryset = Rack.objects.all()
+    serializer_class = RackSerializer
+
+class RackDestroyView(generics.DestroyAPIView):
+    queryset = Rack.objects.all()
+    serializer_class = RackSerializer
+
+class RackListView(generics.ListAPIView):
+    queryset = Rack.objects.all()
+    serializer_class = RackSerializer
