d4fc97375ee86dc3ce4cfd8c997b5608175ebb56
ih33 <ih33@duke.edu>
2020-02-01 15:00:05 -0500

    implement basic auth with the token API / session storage

--- app/hyposoft/frontend/src/components/SessionProvider.jsx
+++ app/hyposoft/frontend/src/components/SessionProvider.jsx
@@ -1,28 +1,41 @@
 import React from "react";
-import Cookies from "js-cookie";
 import PropTypes from "prop-types";
 import Session from "../contexts/Session";
-import API, { SESSION_COOKIE_NAME } from "../api/API";
+import API, { TOKEN_KEY } from "../api/API";
 
-function isAuthCookieThere() {
-  return Cookies.get(SESSION_COOKIE_NAME) != null;
+function getToken() {
+  return sessionStorage.getItem(TOKEN_KEY);
+}
+
+function storeToken(token) {
+  sessionStorage.setItem(TOKEN_KEY, token);
+}
+
+function removeToken() {
+  sessionStorage.removeItem(TOKEN_KEY);
 }
 
 const SessionProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(isAuthCookieThere());
+  const [token, setToken] = React.useState(getToken());
 
   function login(username, password) {
-    return API.login(username, password).then(() =>
-      setIsLoggedIn(isAuthCookieThere())
-    );
+    return API.login(username, password).then(token => {
+      if (token) {
+        storeToken(token);
+        setToken(token);
+      }
+    });
   }
 
   function logout() {
-    return API.logout().then(() => setIsLoggedIn(isAuthCookieThere()));
+    return API.logout().then(() => {
+      removeToken();
+      setToken(null);
+    });
   }
 
   return (
-    <Session.Provider value={{ isLoggedIn, login, logout }}>
+    <Session.Provider value={{ login, logout, token }}>
       {children}
     </Session.Provider>
   );
