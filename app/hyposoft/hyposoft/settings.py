e43bce94f2dde21c95bd58ff4244c56a19396197
Brian Nieves <ban20@duke.edu>
2020-02-04 18:30:59 +0000

    add pagination

--- app/hyposoft/hyposoft/settings.py
+++ app/hyposoft/hyposoft/settings.py
@@ -115,7 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50
 }
 
 
