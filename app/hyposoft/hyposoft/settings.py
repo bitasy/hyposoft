f57a4ad7d87354fdd8f8061da18bf387e6a74c6b
Brian Nieves <ban20@duke.edu>
2020-02-04 18:36:16 +0000

    change default pagination class

--- app/hyposoft/hyposoft/settings.py
+++ app/hyposoft/hyposoft/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50
 }
 
