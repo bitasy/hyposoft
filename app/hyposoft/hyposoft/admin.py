e3955e58a8a54f7636d31a9e03c734e90102587e
Brian Nieves <ban20@duke.edu>
2020-02-04 18:04:47 +0000

    change admin view of user mod to add password description

--- app/hyposoft/hyposoft/admin.py
+++ app/hyposoft/hyposoft/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin
 from rest_framework.authtoken.models import Token
 
 admin.site.unregister(Group)
@@ -9,10 +10,19 @@ admin.site.site_title = "Hyposoft Administration"
 admin.site.index_title = "Hyposoft Administration"
 
 
-class UserAdmin(admin.ModelAdmin):
+class MyUserAdmin(UserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email')
-    fields = ('username', 'first_name', 'last_name', 'email')
+    list_filter = ()
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email')
+        })
+    )
 
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, MyUserAdmin)
