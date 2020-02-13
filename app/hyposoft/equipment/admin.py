aaf5cbe51be6584178f558dc0fc7bca87592bb6d
VCM admin account <vcm@vcm-13118.vm.duke.edu>
2020-02-13 18:55:54 -0500

    filters for bulk export added

--- app/hyposoft/equipment/admin.py
+++ app/hyposoft/equipment/admin.py
@@ -1,21 +1,23 @@
-from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
 from .resources import ITModelResource, InstanceResource
 from django.contrib import admin
 from import_export.formats import base_formats
 from .models import ITModel, Instance, Rack
 
 
-class ITModelAdmin(ImportExportModelAdmin):
+class ITModelAdmin(ImportExportActionModelAdmin):
     resource_class = ITModelResource
     formats = (base_formats.CSV,)
+    list_filter = ['vendor', 'model_number']
 
 
 admin.site.register(ITModel, ITModelAdmin)
 
 
-class InstanceAdmin(ImportExportModelAdmin):
+class InstanceAdmin(ImportExportActionModelAdmin):
     resource_class = InstanceResource
     formats = (base_formats.CSV,)
+    list_filter = ['hostname', 'itmodel', 'rack']
 
 
 admin.site.register(Instance, InstanceAdmin)
