12db536970395684d35a067418c5d2f3240b914e
VCM admin account <vcm@vcm-13118.vm.duke.edu>
2020-02-17 18:08:36 -0500

    fix database and merge conflicts

--- app/hyposoft/equipment/admin.py
+++ app/hyposoft/equipment/admin.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 from import_export.admin import ImportExportActionModelAdmin
-from .resources import ITModelResource, InstanceResource
-=======
-from import_export.admin import ImportExportModelAdmin
 from .resources import ITModelResource, AssetResource
->>>>>>> 3e4edaf484bc0216dabcadf5c2dd553cbd56371e
 from django.contrib import admin
 from import_export.formats import base_formats
 from .models import ITModel, Asset, Rack
@@ -19,13 +14,8 @@ class ITModelAdmin(ImportExportActionModelAdmin):
 admin.site.register(ITModel, ITModelAdmin)
 
 
-<<<<<<< HEAD
-class InstanceAdmin(ImportExportActionModelAdmin):
-    resource_class = InstanceResource
-=======
-class AssetAdmin(ImportExportModelAdmin):
+class AssetAdmin(ImportExportActionModelAdmin):
     resource_class = AssetResource
->>>>>>> 3e4edaf484bc0216dabcadf5c2dd553cbd56371e
     formats = (base_formats.CSV,)
     list_filter = ['hostname', 'itmodel', 'rack']
 
