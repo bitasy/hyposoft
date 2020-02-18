9e306166dc1cb252895c3a4c96e63f8e4bb16cf1
Brian Nieves <ban20@duke.edu>
2020-02-18 12:29:06 -0500

    add user permission checks, allow admin to set others as admin

--- app/hyposoft/hyposoft/settings.py
+++ app/hyposoft/hyposoft/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAdminUser
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -114,14 +116,26 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
     #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     #'PAGE_SIZE': 50,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+if not DEBUG:
+    REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = [
+        'rest_framework.permissions.IsAdminUser',
+        ReadOnly
+    ]
 
 
 # Internationalization
