fb178a4b582cdfcebad0e63e1e806e7f449b3fad
VCM admin account <vcm@vcm-13118.vm.duke.edu>
2020-02-23 15:53:11 -0500

    create connection during import

--- app/hyposoft/equipment/resources.py
+++ app/hyposoft/equipment/resources.py
@@ -196,28 +196,15 @@ class NetworkPortResource(resources.ModelResource):
         report_skipped = True
         clean_model_instances = True
 
-    # def before_import_row(self, row, **kwargs):
-    #     my_src_asset = Asset.objects.get(hostname=row['src_hostname'])
-    #     my_src_model = my_src_asset.itmodel
-    #     my_src_port = NetworkPortLabel.objects.get(name=row['src_port'], itmodel=my_src_model)
-    #     if row['dest_hostname'] != '':
-    #         my_dest_asset = Asset.objects.get(hostname=row['dest_hostname'])
-    #         my_dest_model = my_dest_asset.itmodel
-    #         my_dest_port = NetworkPortLabel.objects.get(name=row['dest_port'], itmodel=my_dest_model)
-    #         my_dest_network_port = NetworkPort.objects.create(
-    #             label=my_dest_port,
-    #             asset=my_dest_asset
-    #         )
-    #         my_dest_network_port.save()
-    #         my_network_port = NetworkPort.objects.create(
-    #             label=my_src_port,
-    #             asset=my_src_asset,
-    #             connection=my_dest_network_port
-    #         )
-    #         my_network_port.save()
-    #     else:
-    #         my_network_port = NetworkPort.objects.create(
-    #             label=my_src_port,
-    #             asset=my_src_asset
-    #         )
-    #         my_network_port.save()
+    def after_import_row(self, row, row_result, **kwargs):
+        my_src_asset = Asset.objects.get(hostname=row['src_hostname'])
+        my_src_label = NetworkPortLabel.objects.get(name=row['src_port'], itmodel=my_src_asset.itmodel)
+        my_src_network_port = NetworkPort.objects.get(asset=my_src_asset, label=my_src_label)
+        my_dest_asset = Asset.objects.get(hostname=row['dest_hostname'])
+        my_dest_label = NetworkPortLabel.objects.get(name=row['dest_port'], itmodel=my_dest_asset.itmodel)
+        my_dest_network_port = NetworkPort.objects.create(asset=my_dest_asset, label=my_dest_label)
+        my_src_network_port.connection = my_dest_network_port
+        my_dest_network_port.connection = my_src_network_port
+        my_src_network_port.save()
+        my_dest_network_port.save()
+
