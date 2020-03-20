fdec94279997355b49e05134e1f3ad828101a794
Brian Nieves <ban20@duke.edu>
2020-03-20 15:20:15 -0400

    Add RackRangeCreate API view

--- app/hyposoft/equipment/new_views.py
+++ app/hyposoft/equipment/new_views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views
 from .new_serializers import *
 from .models import *
 
@@ -11,3 +11,39 @@ class ITModelCreate(generics.CreateAPIView):
 class AssetCreate(generics.CreateAPIView):
     queryset = Asset.objects.all()
     serializer_class = AssetSerializer
+
+
+class RackRangeCreate(views.APIView):
+    def post(self, request):
+
+        def next_char(char):
+            if len(char) == 1:
+                if char < "Z":
+                    return chr(ord(char) + 1)
+                else:
+                    return "AA"
+            else:
+                if char[-1] < "Z":
+                    return char[:-1] + next_char(char[-1])
+                else:
+                    return next_char(char[:-1]) + "A"
+
+        r1 = request.data['r1'].upper()
+        r2 = request.data['r2'].upper()
+        c1 = request.data['c1']
+        c2 = request.data['c2']
+
+        in_range = True
+        curr = r1
+
+        while in_range: # Hacky do-while loop
+            for c in range(c1, c2):
+                Rack.objects.create(
+                    datacenter=request.data['datacenter'],
+                    version=request.META.get('HTTP_X_CHANGE_PLAN', 0),
+                    rack=curr + str(c)
+                )
+
+            if curr == r2:
+                break
+            curr = next_char(r1)
--- app/hyposoft/power/signals.py
+++ app/hyposoft/power/signals.py
@@ -10,29 +10,29 @@ from rest_framework import serializers
 
 @receiver(pre_save, sender=Powered)
 def check_pdu(sender, instance, *args, **kwargs):
-    """    num_powered = len(Powered.objects.filter(asset=instance.asset))
-        if instance not in Powered.objects.all() and num_powered == instance.asset.itmodel.power_ports:
-            raise serializers.ValidationError("All the power port connections have already been used.")
-        if instance.pdu.assets.count() > 24:
-            raise serializers.ValidationError("This PDU is already full.")
-        if instance.pdu.rack != instance.asset.rack:
-            raise serializers.ValidationError(
-                "PDU must be on the same rack as the asset.")"""
+    num_powered = len(Powered.objects.filter(asset=instance.asset))
+    if instance not in Powered.objects.all() and num_powered == instance.asset.itmodel.power_ports:
+        raise serializers.ValidationError("All the power port connections have already been used.")
+    if instance.pdu.assets.count() > 24:
+        raise serializers.ValidationError("This PDU is already full.")
+    if instance.pdu.rack != instance.asset.rack:
+        raise serializers.ValidationError(
+            "PDU must be on the same rack as the asset.")
 
 
 @receiver(pre_save, sender=PDU)
 def set_connected(sender, instance, *args, **kwargs):
-    """    if instance.rack.datacenter.abbr.lower() == 'rtp1':
+    if instance.rack.datacenter.abbr.lower() == 'rtp1':
         response = get_pdu(instance.rack.rack, instance.position)
         instance.networked = response[1] < 400
     else:
-        instance.networked = False"""
+        instance.networked = False
 
 
 @receiver(post_save, sender=Rack)
 def add_PDUs(sender, instance, created, *args, **kwargs):
-    """    if created:
-            left = PDU.objects.create(rack=instance, position=PDU.Position.LEFT)
-            left.save()
-            right = PDU.objects.create(rack=instance, position=PDU.Position.RIGHT)
-            right.save()"""
+    if created:
+        left = PDU.objects.create(rack=instance, position=PDU.Position.LEFT)
+        left.save()
+        right = PDU.objects.create(rack=instance, position=PDU.Position.RIGHT)
+        right.save()
