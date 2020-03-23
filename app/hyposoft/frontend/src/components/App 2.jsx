3cd79eaf7415822cc6c887b8bc349bb5a18a67bb
Carter Gay <cag65@duke.edu>
2020-03-23 19:07:11 -0400

    change database in settings.py

---
+++ app/hyposoft/frontend/src/components/App 2.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import LoginPage from "./auth/LoginPage/LoginPage";
+import ManagementPageFrame from "./management/ManagementPageFrame";
+import ModelManagementPage from "./management/ModelManagement/ModelManagementPage";
+import ModelDetailPage from "./management/ModelManagement/ModelDetailPage";
+import AssetManagementPage from "./management/AssetManagement/AssetManagementPage";
+import ReportManagementPage from "./management/ReportManagement/ReportManagementPage";
+import OverviewPage from "./management/Overview/OverviewPage";
+import AssetDetailPage from "./management/AssetManagement/AssetDetailPage";
+import CreateModelPage from "./management/ModelManagement/CreateModelPage";
+import CreateAssetPage from "./management/AssetManagement/CreateAssetPage";
+import RackManagementPage from "./management/RackManagement/RackManagementPage";
+import RackView from "./management/RackManagement/RackView";
+import LogManagementPage from "./management/LogManagement/LogManagementPage";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCurrentUser } from "../redux/session/actions";
+import DatacenterManagementPage from "./management/DatacenterManagment/DatacenterManagementPage";
+
+
+
+export default function App() {
+  return <Routes />;
+}
+
+function Routes() {
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    dispatch(
+      fetchCurrentUser(
+        () => setLoading(false),
+        () => setLoading(false)
+      )
+    );
+  }, []);
+
+  const currentUser = useSelector(s => s.currentUser);
+
+  if (loading) {
+    return <div />;
+  }
+
+  return currentUser ? (
+    <Router>
+      <Switch>
+        <Route exact path="/racks/print_view">
+          <RackView />
+        </Route>
+        <Route>
+          <ManagementPageFrame>
+            <Switch>
+              <Route exact path="/models">
+                <ModelManagementPage />
+              </Route>
+              <Route exact path="/models/create">
+                <CreateModelPage />
+              </Route>
+              <Route exact path="/models/:id">
+                <ModelDetailPage />
+              </Route>
+
+              <Route exact path="/assets">
+                <AssetManagementPage />
+              </Route>
+              <Route exact path="/assets/create">
+                <CreateAssetPage />
+              </Route>
+              <Route exact path="/assets/:id">
+                <AssetDetailPage />
+              </Route>
+
+              <Route exact path="/datacenters">
+                <DatacenterManagementPage />
+              </Route>
+
+              <Route exact path="/racks">
+                <RackManagementPage />
+              </Route>
+
+              <Route exact path="/reports">
+                <ReportManagementPage />
+              </Route>
+
+              <Route exact path="/logs">
+                <LogManagementPage />
+              </Route>
+
+              <Route>
+                <OverviewPage />
+              </Route>
+            </Switch>
+          </ManagementPageFrame>
+        </Route>
+      </Switch>
+    </Router>
+  ) : (
+    <LoginPage />
+  );
+
+
+
+}
+
+const app = document.getElementsByClassName('App'); //returns list
+console.log(" app elements", app);
---
+++ app/hyposoft/frontend/src/redux/actions 2.js
@@ -0,0 +1,49 @@
+import { displayError } from "../global/message";
+
+const crud_prefixes = ["FETCH_ALL", "FETCH", "CREATE", "UPDATE", "REMOVE"];
+const async_suffixes = ["STARTED", "SUCCESS", "FAILURE"];
+
+export function genAsyncActionTypes(action) {
+  return async_suffixes.map(s => `${action}_${s}`);
+}
+
+export function genAsyncAction(actionType, op, args, onSuccess, onFailure) {
+  const [STARTED, SUCCESS, FAILURE] = genAsyncActionTypes(actionType);
+  return dispatch => {
+    dispatch({ type: STARTED });
+    return op(...args).then(
+      res => {
+        dispatch({ type: SUCCESS, res });
+        onSuccess(res);
+      },
+      err => {
+        dispatch({ type: FAILURE, err });
+        onFailure(err);
+      }
+    );
+  };
+}
+
+export function genCRUDActionTypes(entity) {
+  return crud_prefixes.map(s => `${s}_${entity}`);
+}
+
+export const noOp = () => {};
+
+export function genCRUDActions(
+  [FETCH_ALL, FETCH, CREATE, UPDATE, REMOVE], // action types
+  [fetchAll, fetch, create, update, remove] // actual api functions
+) {
+  return [
+    (onSuccess = noOp, onFailure = noOp) =>
+      genAsyncAction(FETCH_ALL, fetchAll, [], onSuccess, onFailure),
+    (id, onSuccess = noOp, onFailure = noOp) =>
+      genAsyncAction(FETCH, fetch, [id], onSuccess, onFailure),
+    (fields, onSuccess = noOp, onFailure = noOp) =>
+      genAsyncAction(CREATE, create, [fields], onSuccess, onFailure),
+    (id, updates, onSuccess = noOp, onFailure = noOp) =>
+      genAsyncAction(UPDATE, update, [id, updates], onSuccess, onFailure),
+    (id, onSuccess = noOp, onFailure = noOp) =>
+      genAsyncAction(REMOVE, remove, [id], onSuccess, onFailure)
+  ];
+}
