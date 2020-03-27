eb8f119976e6b3675ed98758f53428f1effec64b
Brian Nieves <ban20@duke.edu>
2020-03-27 15:31:29 -0400

    Revert "modify powerportlist fix version bug" because rack id should be in the correct version
    This reverts commit 4e7042d7

--- app/hyposoft/power/list_views.py
+++ app/hyposoft/power/list_views.py
@@ -3,14 +3,12 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 
 from equipment.models import Rack
-from hyposoft.utils import get_version
 
 
 class PowerPortList(views.APIView):
     def get(self, request, **kwargs):
         data = []
-        version = get_version(request)
-        rack = Rack.objects.get(id=request.query_params['rack'], version_id=version)
+        rack = Rack.objects.get(id=request.query_params['rack'])
         for pdu in rack.pdu_set.all():
             for i in range(1, 25):
                 asset = pdu.powered_set.filter(plug_number=i).first()
