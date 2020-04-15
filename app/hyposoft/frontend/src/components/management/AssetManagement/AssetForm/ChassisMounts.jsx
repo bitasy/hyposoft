807f8adf9360f49d1e8f2c1fc11600cef12f16b4
Inchan Hwang <ih33@duke.edu>
2020-04-15 00:49:38 -0400

    slot option

--- app/hyposoft/frontend/src/components/management/AssetManagement/AssetForm/ChassisMounts.jsx
+++ app/hyposoft/frontend/src/components/management/AssetManagement/AssetForm/ChassisMounts.jsx
@@ -10,7 +10,7 @@ function ChassisMounts({ siteList }) {
   const { site, rack } = values.location;
 
   const [rackList, setRackList] = React.useState([]);
-  const [chassisList, setChassisList] = React.useState([]);
+  const [rackViewData, setRackViewData] = React.useState([]);
 
   React.useEffect(() => {
     if (site) {
@@ -20,13 +20,7 @@ function ChassisMounts({ siteList }) {
 
   React.useEffect(() => {
     if (rack) {
-      getRackViewData([rack]).then(res =>
-        setChassisList(
-          res[rack]
-            .filter(({ model }) => model.type === "chassis")
-            .map(({ asset }) => asset),
-        ),
-      );
+      getRackViewData([rack]).then(res => setRackViewData(res[rack]));
     }
   }, [rack]);
 
@@ -40,9 +34,28 @@ function ChassisMounts({ siteList }) {
     return { value: id, text: rack };
   });
 
+  const chassisList = rackViewData
+    .filter(({ model }) => model.type === "chassis")
+    .map(({ asset }) => asset);
+
   const chassisOptions = chassisList.map(({ id, hostname, asset_number }) => {
     return { value: id, text: hostname || asset_number.toString() };
   });
+
+  const slotsTaken = rackViewData
+    .filter(
+      ({ model, asset }) =>
+        model.type === "blade" && asset.id === values.location.asset,
+    )
+    .map(({ asset }) => asset.location.slot);
+
+  const slotOptions = Array(14)
+    .fill(null)
+    .map((v, idx) => idx + 1) // slots 1-based?
+    .filter(n => !slotsTaken.includes(n))
+    .map(n => {
+      return { value: n, text: n };
+    });
 
   return (
     <div>
@@ -53,10 +66,18 @@ function ChassisMounts({ siteList }) {
         <Select name="location.rack" options={rackOptions} />
       </ItemWithLabel>
       <ItemWithLabel name="location.asset" label="Chassis">
-        <Select name="location.asset" options={chassisOptions} />
+        <Select
+          name="location.asset"
+          options={chassisOptions}
+          disabled={!values.location.rack}
+        />
       </ItemWithLabel>
-      <ItemWithLabel name="location.slot" label="Slot">
-        <InputNumber name="location.slot" min={1} max={14} />
+      <ItemWithLabel name="location.slot" label="Available slots">
+        <Select
+          name="location.slot"
+          options={slotOptions}
+          disabled={!values.location.asset}
+        />
       </ItemWithLabel>
     </div>
   );
