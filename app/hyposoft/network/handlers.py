e31a48e13010700dd4a9c5142a3fb71a9df36154
Brian Nieves <ban20@duke.edu>
2020-04-17 17:07:13 -0400

    fix network graph for blades

--- app/hyposoft/network/handlers.py
+++ app/hyposoft/network/handlers.py
@@ -22,7 +22,25 @@ def net_graph(asset_id):
         b = edge.connection.asset.id
         return min(a, b), max(a, b)
 
+    blade_nodes = []
+    blade_edges = []
+
+    def add_asset_blades(asset):
+        blades = asset.blade_set.all()
+        if blades.exists():
+            for blade in blades:
+                blade_nodes.append({
+                    'id': blade.id,
+                    'label': "{}: Slot {}".format(blade.hostname, blade.slot) if blade.hostname else
+                             "Slot " + blade.slot + " blade"
+                })
+                blade_edges.append((asset.id, blade.id))
+
+    add_asset_blades(asset)
+    for asset in assets:
+        add_asset_blades(asset)
+
     return {
-        'verticies': [{"id": asset.id, "label": str(asset)} for asset in two_hops],
-        'edges': set([orderedEdge(edge) for edge in edges if edge.connection])
+        'verticies': [{"id": asset.id, "label": str(asset)} for asset in two_hops] + blade_nodes,
+        'edges': set([orderedEdge(edge) for edge in edges if edge.connection] + blade_edges)
     }
